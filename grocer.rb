b46bd4b4f3c7e590c8aab5e0f2e3cbffe02e4dd6
Daniel Steed <dsteed112@gmail.com>
2020-03-21 02:39:03 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -56,11 +56,11 @@ def apply_coupons(cart, coupons)
   while coupons_index < coupons.size do
     current_coupon = coupons[coupons_index]
     applicable_for_discount = find_item_by_name_in_collection(current_coupon[:item], cart)
-      if (applicable_for_discount[:count] / current_coupon[:num] >= 1 )
+      if (applicable_for_discount[:count] / current_coupon[:num] >= 1)
         cart.push( {:item => "#{current_coupon[:item]} W/COUPON",
                     :price => (current_coupon[:cost] / current_coupon[:num]).round(2),
                     :clearance => applicable_for_discount[:clearance],
-                    :count => applicable_for_discount[:count] - ( applicable_for_discount[:count] % current_coupon[:num])})
+                    :count => applicable_for_discount[:count] - (applicable_for_discount[:count] % current_coupon[:num])})
           
         applicable_for_discount[:count] %= current_coupon[:num]
       end
